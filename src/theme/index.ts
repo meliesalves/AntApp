b436856e23d1db14f9bd3f9110c461a6c392abca
Melies Kubrick <melieskubrick@gmail.com>
2023-02-23 12:41:39 -0300

    feat: create theme

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: "#202024",
+    secondary: "#FFFFFF",
+    black: "#000000",
+    gray_light: "#8E8E8E",
+    gray: "#29292E",
+    gray_dark: "#121214",
+    red: "#FF0000",
+  },
+
+  typography: {
+    light: "Poppins_300Light",
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    semi_bold: "Poppins_600SemiBold",
+    bold: "Poppins_700Bold",
+  },
+};
