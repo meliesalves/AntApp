11e3d99417ab32deb049090c9952b1b5482e82aa
Melies Kubrick <melieskubrick@gmail.com>
2023-02-23 13:11:50 -0300

    feat: add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: true,
+          allowUndefined: true,
+        },
+      ],
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@src": "./src",
+            "@types": "./src/@types",
+          },
+        },
+      ],
+    ],
   };
 };
